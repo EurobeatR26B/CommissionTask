5ece8cb923badef4b71e24886d45a5cf7c89b1b3
Justas Gabulas <gabulas.justas@gmail.com>
2024-08-18 23:18:52 +0300

    The calculations are finally correct

--- src/CommissionCalculation/PrivateWithdrawRule.php
+++ src/CommissionCalculation/PrivateWithdrawRule.php
@@ -33,57 +33,6 @@ class PrivateWithdrawRule implements CommissionRuleInterface
         return $commission;
     }
 
-    // private function getTaxableAmount (Operation $operation): float
-    // {
-    //     $taxableAmount = $operation->getAmount();
-    //     $isUserEligibleForFreeCommission = $this->userOperationTracker->isOperationEligibleForFreeCommission($operation);
-
-    //     if ($isUserEligibleForFreeCommission)
-    //     {
-    //         if ($operation->getCurrency() == FREE_COMMISSION_PRIVATE_USER_WITHDRAW_CURRENCY)
-    //         {
-    //             $userOperationSum = $this->userOperationTracker->getUserOperationSumThisPeriod($operation);
-    //             $remainingTaxCredits = FREE_COMMISSION_PRIVATE_USER_WITHDRAW_AMOUNT - $userOperationSum;
-
-    //             $taxableAmount = $operation->getAmount() >= $remainingTaxCredits ? 
-    //                 $operation->getAmount() - $remainingTaxCredits :
-    //                 0.00;
-
-    //             // $taxableAmount = $operation->getAmount() - FREE_COMMISSION_PRIVATE_USER_WITHDRAW_AMOUNT;
-    //             $taxableAmount = $taxableAmount >= 0 ? $taxableAmount : 0.00;
-            
-    //         }
-
-    //         else if ($operation->getCurrency() !== FREE_COMMISSION_PRIVATE_USER_WITHDRAW_CURRENCY)
-    //         {
-    //             $operationAmountInMainCurrency = $this->userOperationTracker->currencyConverter->convertOperation(
-    //                 $operation,
-    //                 FREE_COMMISSION_PRIVATE_USER_WITHDRAW_CURRENCY
-    //             );
-
-    //             echo "Converted " . $operation->getAmount() . " " . $operation->getCurrency() . " to $operationAmountInMainCurrency EUR" . PHP_EOL;
-
-    //             $remainingCredits = FREE_COMMISSION_PRIVATE_USER_WITHDRAW_AMOUNT - $this->userOperationTracker->getUserOperationSumThisPeriod($operation);
-
-    //             if ($operationAmountInMainCurrency > $remainingCredits)
-    //             {
-    //                 $taxableAmountInMainCurrency = $operationAmountInMainCurrency - $remainingCredits;
-
-    //                 $taxableAmountInOperationCurrency = $taxableAmountInMainCurrency * 
-    //                 $this->userOperationTracker->currencyConverter->getExchangeRate("EUR", $operation->getCurrency());
-
-    //                 $taxableAmount = $taxableAmountInOperationCurrency;
-    //             }
-    //             else 
-    //             {
-    //                 $taxableAmount = 0.00;
-    //             }
-    //         }
-    //     }
-
-    //     return $taxableAmount;
-    // }
-
     private function getTaxableAmount (Operation $operation): float
     {
         $taxableAmount = $operation->getAmount();
@@ -111,13 +60,19 @@ class PrivateWithdrawRule implements CommissionRuleInterface
             $exchangeRate = $this->userOperationTracker->currencyConverter->getExchangeRate($operation->getCurrency(), FREE_COMMISSION_PRIVATE_USER_WITHDRAW_CURRENCY);
 
             $operationInMainCurrency = $operation->getAmount() * $exchangeRate;
-            
             if ($operationInMainCurrency > $remainingTaxCredits)
             {
-                $amountToTaxInMainCurrency = $operationInMainCurrency - $remainingTaxCredits;
-                $amountToTaxInOperationCurrency = $amountToTaxInMainCurrency / $exchangeRate;
+                if ($remainingTaxCredits <= 0)
+                {
+                    $taxableAmount = $operation->getAmount();
+                }
+                else 
+                {
+                    $amountToTaxInMainCurrency = $operationInMainCurrency - $remainingTaxCredits;
+                    $amountToTaxInOperationCurrency = $amountToTaxInMainCurrency / $exchangeRate;
 
-                $taxableAmount = $amountToTaxInOperationCurrency;
+                    $taxableAmount = $amountToTaxInOperationCurrency;
+                }                
             }
             else 
             {
